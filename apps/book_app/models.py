059ec098f94ea05938b1ef37de2896f6fec453c8
Scott <marden.scott@gmail.com>
2017-06-20 15:48:00 -0700

    Finished assignment. Built model for books, added data via python shell, added new field, and made a call to the database to print all book info to console.

--- apps/book_app/models.py
+++ apps/book_app/models.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
 
 # Create your models here.
+class Book(models.Model):
+	title = models.TextField()
+	author = models.CharField(max_length=255)
+	published_date = models.DateField()
+	category = models.CharField(max_length=255)
+	in_print = models.BooleanField()
--- apps/book_app/views.py
+++ apps/book_app/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
+from .models import Book
 
 # Create your views here.
 def index(request):
+	#Adding data to books model in python shell
+	books = Book.objects.all()
+	for book in books:
+		print book.title, book.author, book.published_date, book.category, book.in_print
 	return render(request, 'book_app/index.html')
